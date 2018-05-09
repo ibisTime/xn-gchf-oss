91f985af894dcda94b1cb55ede5f5692b2933c23
1IOZ2QJVGODSMF7\Administrator <463562667@qq.com>
2018-05-09 19:44:45 +0800

    bug 页面改进

--- src/api/downLoad.js
+++ src/api/downLoad.js
@@ -4,3 +4,12 @@ import fetch from 'common/js/fetch';
 export function downLoad(messageCode) {
   return fetch(631446, { 'messageCode': messageCode });
 }
+
+// 详情页查询数据
+export function detailDate(code) {
+  return fetch(631437, { code });
+}
+
+export function downNum(code) {
+  return fetch(631431, { code });
+}
--- src/container/waitList/alreadyQuest-addedit/alreadyQuest-addedit.js
+++ src/container/waitList/alreadyQuest-addedit/alreadyQuest-addedit.js
@@ -9,9 +9,10 @@ import {
 } from '@redux/waitList/alreadyQuest-addedit';
 import { getQueryString, showSucMsg, formatDate, showWarnMsg } from 'common/js/util';
 import { DetailWrapper } from 'common/js/build-detail';
-import { Button } from 'antd';
-import { downLoad } from 'api/downLoad';
+import { Button, Card } from 'antd';
+import { downLoad, detailDate } from 'api/downLoad';
 import XLSX from 'xlsx';
+import { div } from 'gl-matrix/src/gl-matrix/vec4';
 
 @DetailWrapper(
   state => state.waitListAlreadyQuestAddedit,
@@ -24,12 +25,36 @@ class AlreadyQuestAddedit extends React.Component {
     this.code = getQueryString('code', this.props.location.search);
     this.view = !!getQueryString('v', this.props.location.search);
     this.handleExport = this.handleExport.bind(this);
+    this.handleExport1 = this.handleExport1.bind(this);
     this.state = {
       data: [],
-      cols: []
+      cols: [],
+      bankcardNumber: ' ',
+      handleDatetime: ' ',
+      bankName: ' ',
+      download: ' ',
+      projectName: ' ',
+      sendDatetime: '',
+      downLoad: ''
     };
   };
-  handleExport() {
+  componentDidMount() {
+    detailDate(this.code).then((data) => {
+      console.log(data);
+      this.setState({
+        bankcardNumber: data.bankcardNumber,
+        handleDatetime: data.handleDatetime,
+        bankName: data.bankName,
+        projectName: data.projectName,
+        sendDatetime: data.sendDatetime,
+        downLoad: data.downLoad
+      });
+    });
+  }
+  goBack() {
+    history.go(-1);
+  }
+  handleExport1(argu) {
     downLoad(this.code).then((data) => {
       let payroll1 = [
         ['项目信息'],
@@ -48,52 +73,43 @@ class AlreadyQuestAddedit extends React.Component {
       XLSX.writeFile(wb, 'sheetjs.xlsx');
     }, () => { });
   }
+  handleExport(argu) {
+    downLoad(this.code).then((data) => {
+      let payroll1 = [
+        ['项目信息'],
+        ['项目编号', data[1].projectCode],
+        ['扣款账户', data[1].bankCard.bankName],
+        ['代付工资信息'],
+        ['序号', '工资条编号', '真实姓名', '开户行', '卡号', '应发金额', '已发金额', '发放时间']
+      ];
+      let payroll2 = data.map((d, i) => {
+        return [i + 1, d.code, d.bankCard.staffName, d.bankCard.bankName, d.bankCard.bankcardNumber, d.shouldAmount, '', ''];
+      });
+      payroll1 = payroll1.concat(payroll2);
+      const ws = XLSX.utils.aoa_to_sheet(payroll1);
+      const wb = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, 'SheetJS');
+      XLSX.writeFile(wb, 'sheetjs.xlsx');
+    }, () => { });
+  }
   render() {
-    const fields = [{
-      field: 'handler',
-      hidden: true
-    }, {
-      title: '请求时间',
-      field: 'sendDatetime',
-      type: 'datetime'
-    }, {
-      title: '标题',
-      field: 'title'
-    }, {
-      title: '代发账户户名',
-      field: 'bankName'
-    }, {
-      title: '代发账户账号',
-      field: 'bankcardNumber'
-    }, {
-      title: '代发工资条下载',
-      field: 'download1',
-      type: 'download',
-      handler: this.handleExport
-    }, {
-      title: '下载次数',
-      field: 'download'
-    }, {
-      title: '标题',
-      field: 'title1',
-      _keys: ['title'],
-      formatter: (v, d) => {
-        return v + '反馈';
-      }
-    }, {
-      title: '反馈时间',
-      field: 'handleDatetime',
-      type: 'datetime'
-    }];
-    return this.props.buildDetail({
-      fields,
-      code: this.code,
-      view: this.view,
-      searchParams: {
-        status: 3
-      },
-      detailCode: 631437
-    });
+    return (
+      <div>
+        <Card title={this.state.projectName + '123'} style={{ width: 500 }}>
+          <p>请求时间：{formatDate(this.state.sendDatetime)}</p>
+          <p>代发账户户名：{this.state.bankName}</p>
+          <p>代发账户账号：{this.state.bankcardNumber}</p>
+          <button onClick={this.handleExport}>点击下载</button>
+          <p>下载次数{this.state.downLoad}</p>
+        </Card>
+        <Card title={this.state.projectName + '工资反馈'} style={{ width: 500, marginTop: 20 }}>
+          <p>反馈时间：{formatDate(this.state.handleDatetime)}</p>
+          <button onClick={this.handleExport1}>点击下载</button>
+          <p>下载次数{this.state.downLoad}</p>
+        </Card>
+        <Button onClick={this.goBack.bind(this)} style={{ marginTop: 20 }}>返回</Button>
+      </div>
+    );
   }
 }
 
