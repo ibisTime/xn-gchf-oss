0e8f5aced8cbf371d827489fe7ff0c8151b4f910
clare29 <1397452342@qq.com>
2018-07-20 14:02:05 +0800

    update

--- src/container/staff/allStaff/allStaff.js
+++ src/container/staff/allStaff/allStaff.js
@@ -62,6 +62,9 @@ class AllStaff extends React.Component {
       field: 'mobile',
       title: '手机号'
     }, {
+      field: 'remark',
+      title: '备注'
+    }, {
       field: 'birthdays',
       title: '生日',
       type: 'datetime',
@@ -74,9 +77,6 @@ class AllStaff extends React.Component {
       placeholder: '名字/证件号',
       hidden: true,
       search: true
-    }, {
-      field: 'remark',
-      title: '备注'
     }];
     const btnEvent = {
       error: (selectedRowKeys, selectedRows) => {
